e30986b73f6267e1f514f0e16042eb000a64b965
Mert Rasna <mrasnalu02@gmail.com>
2025-10-26 09:45:26 +0100

    step2

---
+++ part1/server/CalcImpl.java
@@ -0,0 +1,40 @@
+import java.rmi.RemoteException;
+public class CalcImpl implements ICalc {
+  public CalcImpl() {}
+  @Override
+  public int factorial(int n) throws RemoteException,
+         InvalidInputException {
+           if (n < 0) {
+             throw new InvalidInputException("factorial: n must be >= 0
+                 (got " + n + ")");
+           }
+           int res = 1;
+           for (int i = 2; i <= n; i++) res *= i;
+           return res;
+  }
+  @Override
+  public double sum(double a, double b) throws RemoteException {
+    return a + b;
+  }
+  @Override
+  public double product(double a, double b) throws RemoteException {
+    return a * b;
+  }
+  @Override
+  public double sum2(Args args) throws RemoteException {
+    // To be implemented
+    return 0.0;
+  }
+  @Override
+  public double product2(Args args) throws RemoteException {
+    // To be implemented
+    return 0.0;
+  }
+  @Override
+  public double sqrt(double x) throws RemoteException,
+         InvalidInputException {
+           // To be implemented: make sure to use the invalid input
+           exception
+             return 0.0;
+  }
+}
---
+++ part1/server/Server.java
@@ -0,0 +1,24 @@
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+import java.rmi.server.UnicastRemoteObject;
+public class Server {
+  public static void main(String[] args) {
+    try {
+      String name = "calc";
+      CalcImpl impl = new CalcImpl();
+      // Export as a unicast remote object (auto-creates the
+      stub).
+        ICalc stub = (ICalc)
+        UnicastRemoteObject.exportObject(impl, 0);
+      // Create and bind a registry locally.
+      Registry registry =
+        LocateRegistry.createRegistry(Registry.REGISTRY_PORT);
+      registry.rebind(name, stub);
+      System.out.println("RMI Calculator server ready (bound as
+        \"" + name + "\")");
+    } catch (Exception e) {
+      System.err.println("Server exception:");
+      e.printStackTrace();
+    }
+  }
+}
